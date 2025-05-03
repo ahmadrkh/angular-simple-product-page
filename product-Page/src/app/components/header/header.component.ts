72881f98da8387986cd45971b16f3c86750f0c0a
arezakhanari <a.reza.khanri@gmail.com>
2025-05-03 12:54:46 +0330

    Add static header

---
+++ product-Page/src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
